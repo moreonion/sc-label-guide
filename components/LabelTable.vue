d8f59f067810157bb489929aa201af700e24e077
dinony <contact@dinony.com>
2017-08-01 12:39:57 +0200

    Prep label table component

---
+++ components/LabelTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <table>
+    <thead>
+      <th>Labels</th>
+      <th>Governance & Transparency</th>
+      <th>Environmental impact</th>
+      <th>Social impact</th>
+    </thead>
+    <tbody>
+      <tr><td>RSPO</td><td>1</td><td>2</td><td>3</td></tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Label Guide',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -21,6 +21,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    vendor: ['mo-vue-table'],
     /*
     ** Run ESLINT on save
     */
