ce0eefbe6f05af55d72b0d30f1a8edf58be77fd8
Onur D <contact@dinony.com>
2017-08-05 19:25:27 +0200

    Fix bgInfo close btn

--- components/LabelTable.vue
+++ components/LabelTable.vue
@@ -76,7 +76,7 @@
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
       </p>
       <span slot="footer">
-        <btn @click="hideInfoDialog">Close</btn>
+        <btn @click="dialog['bgInfo'].visible = false">Close</btn>
       </span>
     </el-dialog>
   </div>
