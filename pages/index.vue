2beb25c3af2ddb5ad2761cf96f380e3d575038cb
Onur D <contact@dinony.com>
2017-08-07 16:21:26 +0200

    Move rnd data gen in lib dir

--- pages/index.vue
+++ pages/index.vue
@@ -6,33 +6,8 @@
   import axios from 'axios'
   import LabelTable from '../components/LabelTable.vue'
 
-  // const labels = [
-  //   {name: 'RSPO', img: 'logos/RSPO.png'},
-  //   {name: 'EU Organic', img: 'logos/EU.jpg'},
-  //   {name: 'Fairtrade', img: 'logos/Fairtrade.png'},
-  //   {name: 'UTZ', img: 'logos/UTZ.png'}
-  // ]
-  //
-  // const rndLabel = () => labels[Math.floor(Math.random() * labels.length)]
-  // const rndRating = () => Math.ceil(Math.random() * 3)
-  //
-  // const genTableData = num => {
-  //   const res = []
-  //   for (let i = 0; i < num; i++) {
-  //     res.push({
-  //       label: rndLabel(),
-  //       govTrans: rndRating(),
-  //       envImpact: rndRating(),
-  //       scoImpact: rndRating()
-  //     })
-  //   }
-  //   return res
-  // }
-
   export default {
-    components: {
-      LabelTable
-    },
+    components: {LabelTable},
     async asyncData ({isServer, req, isDev, route}) {
       const protocol = isDev ? 'http' : 'https'
       let res = {data: []}
