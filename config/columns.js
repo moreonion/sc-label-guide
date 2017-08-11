7c516898029fbf2d50dbfab6d1fb41e810cc593c
Onur D <contact@dinony.com>
2017-08-11 13:32:42 +0200

    Split column and operator config

---
+++ config/columns.js
@@ -0,0 +1,34 @@
+const types = {
+  'RATING': 'rating',
+  'TEXT': 'text'
+}
+
+export const _COLUMNS_ = {
+  types,
+  // Columns with order
+  columns: [['label', 0], ['govTrans', 1], ['envImpact', 2], ['socImpact', 3]],
+  columnValueMap: {
+    'label': 'label.name',
+    'govTrans': 'govTrans',
+    'envImpact': 'envImpact',
+    'socImpact': 'socImpact'
+  },
+  columnValFuncMap: {
+    'label.name': row => row.label.name,
+    'govTrans': row => row.govTrans,
+    'envImpact': row => row.envImpact,
+    'socImpact': row => row.socImpact
+  },
+  columnLabelMap: {
+    'label.name': 'Label',
+    'govTrans': 'Governance& Transparency',
+    'envImpact': 'Environmental impact',
+    'socImpact': 'Social impact'
+  },
+  columnMeta: {
+    'label.name': {type: types.TEXT, hasInfo: true, mandatory: true},
+    'govTrans': {type: types.RATING},
+    'envImpact': {type: types.RATING},
+    'socImpact': {type: types.RATING}
+  }
+}
