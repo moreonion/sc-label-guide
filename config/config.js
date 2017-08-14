7deac0f3361bb29feef61ffed83fec31d16fe309
Onur D <contact@dinony.com>
2017-08-14 15:08:20 +0200

    Make query delim params configable

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export {_OPERATORS_} from './operators.js'
 export {_COLUMNS_} from './columns.js'
+export {_ROUTE_} from './routeQuery.js'
---
+++ config/routeQuery.js
@@ -0,0 +1,8 @@
+/**
+ * E.g.: ?gt=label-3,govTrans-1
+ * Is the route query serialization of: label > 3 && govTrans > 1
+ */
+export const _ROUTE_ = {
+  queryDelim: ',',
+  querySubDelim: '-'
+}
