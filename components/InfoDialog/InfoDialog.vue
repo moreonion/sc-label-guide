7af2ed0977b4357d954a9e0fe142429f2ff17cef
Onur D <contact@dinony.com>
2017-08-15 18:12:26 +0200

    Prep InfoDialog

--- components/InfoDialog/InfoDialog.vue
+++ components/InfoDialog/InfoDialog.vue
@@ -1,11 +1,21 @@
 <template>
   <el-dialog :visible="visible" @update:visible="updateVisible" @close="onClose" size="large">
-    <span slot="title">Criteria this label has</span>
+    <h1 v-if="label.row" slot="title">{{label.row.name}}</h1>
+    <div v-if="label.row">
+      <section v-if="label.row.description">
+        <h2>Description</h2>
+        <p>{{label.row.description}}</p>
+      </section>
 
-    <section v-if="label.row && label.row.description">
-      <h2>Description</h2>
-      <p>{{label.row.description}}</p>
-    </section>
+      <section v-if="label.row.meets_criteria">
+        <h2>Criteria met</h2>
+        <ul>
+          <li class="criterion" v-for="c in label.row.meets_criteria">
+            {{criterionDescr(c)}}
+          </li>
+        </ul>
+      </section>
+    </div>
 
     <span slot="footer">
       <el-button @click="dismiss">Close</el-button>
@@ -22,11 +32,14 @@ export default {
   methods: {
     onClose: function() {
       this.dismiss()
-    }
+    },
+    criterionDescr: c => c.criterion.details.measures[c.score+'']
   }
 }
 </script>
 
 <style>
-
+  .criterion {
+    margin: 20px 0px;
+  }
 </style>
