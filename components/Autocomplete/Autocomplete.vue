3a35254135be1aefce7fec62b1752e8eb125e261
Onur D <contact@dinony.com>
2017-08-29 13:16:30 +0200

    Prepare autocomplete component

--- components/Autocomplete/Autocomplete.vue
+++ components/Autocomplete/Autocomplete.vue
@@ -1,21 +1,48 @@
 <template>
   <el-autocomplete
-    class="inline-input"
-    v-model="state1"
+    v-model="state"
     :fetch-suggestions="querySearch"
-    placeholder="Please Input"
-    @select="handleSelect">
+    placeholder="Please input"
+    @select="handleSelect"
+    :props="{label:'name'}">
   </el-autocomplete>
 </template>
 
 <script>
-export default {
-  methods: {
-    querySearch(queryString, cb) {
-      cb([1,2,3])
+  import axios from 'axios'
+  import {_APIURL_} from '../../lib/api/base.js'
+
+  export default {
+    props: ['config'],
+    data: () => ({state: null}),
+    methods: {
+      querySearch(queryString, cb) {
+        if(this.config.sync) {
+          cb(this.config.sync)
+        } else {
+          let params = {}
+          const mw = this.config.middlewares
+          if(mw && mw.prefetch) {
+            params = mw.prefetch(queryString)
+          }
+
+          axios.get(`${_APIURL_}/${this.config.async}`, {params}).then(res => {
+            if(mw && mw.postfetch) {
+              cb(mw.postfetch(res))
+            } else {
+              cb(res.data)
+            }
+          }).catch(err => {
+            if(err) cb([])
+            else cb([])
+          })
+        }
+      },
+      handleSelect() {
+
+      }
     }
   }
-}
 </script>
 
 <style>
