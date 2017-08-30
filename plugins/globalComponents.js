b12b917c6f0abb80fb88581e736f11622cfce051
Onur D <contact@dinony.com>
2017-08-30 11:30:18 +0200

    Add global component

--- plugins/globalComponents.js
+++ plugins/globalComponents.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 
+import EvalCircle from '../components/Eval/EvalCircle.vue'
 import {EvalDropdownItem} from '../components/Eval/EvalDropdownItem.js'
+
+Vue.component('eval-circle', EvalCircle)
 
 // Needs global registration due to el-autocomplete
 Vue.component('eval-dropdown-item', EvalDropdownItem)
