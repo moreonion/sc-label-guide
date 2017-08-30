e1ea981d3524d41285e3bfae55017fba5d620ae1
Onur D <contact@dinony.com>
2017-08-30 13:37:24 +0200

    Prep autocomplete function

--- config/api.js
+++ config/api.js
@@ -12,14 +12,14 @@ export const _API_ = {
 /**
  * A default autocomplete endpoint returns paged results to given
  * query.
- * 
+ *
  * An emulated autocomplete endpoint returns paged results without
  * considering a query.
  * That is an emulated autocompletion may be implemented the following way:
- * 
+ *
  * 1. Fetch endpoint with offset=0 and limit={limit}
  * 2. On the client: filter response data for given query
- * 3. If filtered results is less than {minResults} then 
+ * 3. If filtered results is less than {minResults} then
  *    add the filtered results of the next page to the current
  *    result set until either enough results are found or no
  *    more pages are left. Otherwise return filtered results.
