383b2e36e373fed6fa376ff1d88dd4a5d7326184
Onur D <contact@dinony.com>
2017-09-04 15:04:38 +0200

    Adapt lang middleware

--- middleware/lang.js
+++ middleware/lang.js
@@ -1,25 +1,32 @@
 import locale from 'locale'
+// use locale2 to detect browser lang / node lang
+import locale2 from 'locale2'
+
 import {_DEFAULT_LANGUAGE_, _AVAILABLE_LANGUAGES_} from '../config/language.js'
 import {SET_LANG} from '../store/mutation-types.js'
 // import D from '../lib/debug.js'
 
 locale.Locale['default'] = _DEFAULT_LANGUAGE_
 
-export default function({isServer, store, query, req}) {
+export default function({isClient, isServer, store, query, req}) {
   const supportedLanguages = new locale.Locales(_AVAILABLE_LANGUAGES_, _DEFAULT_LANGUAGE_)
-
-  let acceptLang = null
-  if(isServer && req.headers['accept-language']) {
-    const locales = new locale.Locales(req.headers['accept-language'])
-    acceptLang = locales.best(supportedLanguages)
-  }
 
   let routeLang = null
   if(query && query.lang) {
-    routeLang = supportedLanguages.best(new locale.Locales([query.lang]))
+    const queryLocale = new locale.Locales([query.lang], _DEFAULT_LANGUAGE_)
+    routeLang = queryLocale.best(supportedLanguages)
   }
 
-  const bestLang = routeLang || acceptLang || _DEFAULT_LANGUAGE_
+  let acceptLang = null
+  if(isServer && req.headers['accept-language']) {
+    const acceptedLocales = new locale.Locales(req.headers['accept-language'], _DEFAULT_LANGUAGE_)
+    acceptLang = acceptedLocales.best(supportedLanguages)
+  }
+
+  const clientLocale = new locale.Locales([locale2], _DEFAULT_LANGUAGE_)
+  const clientLang = clientLocale.best(supportedLanguages)
+
+  const bestLang = routeLang || acceptLang || clientLang || _DEFAULT_LANGUAGE_
 
   store.commit(SET_LANG, bestLang)
 }
