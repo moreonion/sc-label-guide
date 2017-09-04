3ad781f012ca91a222b252b08f0be8f7d7c53488
Onur D <contact@dinony.com>
2017-09-04 15:03:57 +0200

    Import german lang

--- plugins/vue-i18n.js
+++ plugins/vue-i18n.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 import en from '../static/i18n/lang_en.json'
+import de from '../static/i18n/lang_de.json'
 
 Vue.use(VueI18n)
 
-const translations = {'en': en}
+const translations = {'en': en, 'de': de}
 
 export default ({app}) => {
   app.i18n = new VueI18n({
