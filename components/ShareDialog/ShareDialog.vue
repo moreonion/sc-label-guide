e76969417e4afa1dcc44e01090471af4741fb825
Onur D <contact@dinony.com>
2017-09-04 10:35:55 +0200

    Add translation

--- components/ShareDialog/ShareDialog.vue
+++ components/ShareDialog/ShareDialog.vue
@@ -1,9 +1,9 @@
 <template>
   <el-dialog :visible="visible" @update:visible="updateVisible" @close="onClose" size="large">
-    <span slot="title">Embed this label guide on your website</span>
+    <span slot="title">{{$t('Texts.Embed')}}</span>
     <el-input readonly type="textarea" :rows="15" v-model="shareSnippet"></el-input>
     <span slot="footer">
-      <el-button @click="dismiss">Close</el-button>
+      <el-button @click="dismiss">{{$t('Buttons.Close')}}</el-button>
     </span>
   </el-dialog>
 </template>
