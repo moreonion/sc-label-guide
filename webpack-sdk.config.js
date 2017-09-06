9652ca8f9f3f8fb790dafbd56fc5f3fdf37a959a
Onur D <contact@dinony.com>
2017-09-06 19:34:30 +0200

    Update sdk webpack

--- webpack-sdk.config.js
+++ webpack-sdk.config.js
@@ -14,7 +14,7 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'scss': 'vue-style-loader!css-loader!sass-loader'
           }
         }
       },
@@ -24,11 +24,19 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader'
       }
     ]
   },
