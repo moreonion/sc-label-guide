586f7457132b0c4b39faa6cf04df5d7beb295354
Onur D <contact@dinony.com>
2017-09-06 17:55:02 +0200

    Prep sdk webpack config

---
+++ webpack-sdk.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: './sdk/src/index.js',
+  output: {
+    path: path.resolve(__dirname, './sdk-dist'),
+    filename: 'sdk.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  performance: {hints: false},
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.performance.hints = false
+
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
