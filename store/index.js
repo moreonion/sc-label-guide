21de695e5e8d56e1e500cf3cbb76e85fa320cdb3
Onur D <contact@dinony.com>
2017-09-15 10:52:43 +0200

    Add detected lang in store

--- store/index.js
+++ store/index.js
@@ -1,16 +1,20 @@
 import Vuex from 'vuex'
 
 import {_DEFAULT_LANGUAGE_} from '../config/language.js'
-import {SET_LANG} from './mutation-types.js'
+import {SET_LANG, SET_DETECTED_LANG} from './mutation-types.js'
 
 export default () => {
   return new Vuex.Store({
     state: {
-      lang: _DEFAULT_LANGUAGE_
+      lang: _DEFAULT_LANGUAGE_,
+      detectedLang: _DEFAULT_LANGUAGE_
     },
     mutations: {
       [SET_LANG](state, lang) {
         state.lang = lang
+      },
+      [SET_DETECTED_LANG](state, lang) {
+        state.detectedLang = lang
       }
     }
   })
--- store/mutation-types.js
+++ store/mutation-types.js
@@ -1,1 +1,2 @@
 export const SET_LANG = 'SET_LANG'
+export const SET_DETECTED_LANG = 'SET_DETECTED_LANG'
