d89886f3899b3e8b75eba5d6b512f73fc7642710
Onur D <contact@dinony.com>
2017-09-26 18:28:53 +0200

    Responsive layout for table legend

--- components/TableLegend.vue
+++ components/TableLegend.vue
@@ -1,28 +1,42 @@
 <template>
-  <el-row type="flex" algin="middle" class="legend-cont">
-    <el-col :xs="8" :sm="5">
-      <div class="eval-cont">
-        <eval-circle value="3"></eval-circle><span>{{$t('Evaluation.Good')}}</span>
-      </div>
+  <el-row class="legend-cont">
+    <el-col :xs="24" :sm="20">
+      <el-row>
+        <el-col :span="6">
+          <div class="eval-cont">
+            <eval-circle value="3"></eval-circle>
+            <span class="eval-label">{{$t('Evaluation.Good')}}</span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="eval-cont">
+            <eval-circle value="2"></eval-circle>
+            <span class="eval-label">{{$t('Evaluation.Mediocre')}}</span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="eval-cont">
+            <eval-circle value="1"></eval-circle>
+            <span class="eval-label">{{$t('Evaluation.NotGood')}}</span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="eval-cont">
+            <eval-circle value="0"></eval-circle>
+            <span class="eval-label">{{$t('Evaluation.Unknown')}}</span>
+          </div>
+        </el-col>
+      </el-row>
     </el-col>
-    <el-col :xs="8" :sm="5" class="test">
-      <div class="eval-cont">
-        <eval-circle value="2"></eval-circle><span>{{$t('Evaluation.Mediocre')}}</span>
-      </div>
-    </el-col>
-    <el-col :xs="8" :sm="5" class="test">
-      <div class="eval-cont">
-        <eval-circle value="1"></eval-circle><span>{{$t('Evaluation.NotGood')}}</span>
-      </div>
-    </el-col>
-    <el-col :xs="8" :sm="5" class="test">
-      <div class="eval-cont">
-        <eval-circle value="0"></eval-circle><span>{{$t('Evaluation.Unknown')}}</span>
-      </div>
-    </el-col>
-    <el-col :xs="24" :sm="8">
-      <span>{{$t('Questions.Scoring')}}</span><br>
-      <a @click="onClick">{{$t('Buttons.BackgroundInformation')}}</a>
+    <el-col :xs="24" :sm="4">
+      <el-row>
+        <el-col>
+          <div class="bginfo-wrapper">
+            <span>{{$t('Questions.Scoring')}}</span><br>
+            <a @click="onClick">{{$t('Buttons.BackgroundInformation')}}</a>
+          </div>
+        </el-col>
+      </el-row>
     </el-col>
   </el-row>
 </template>
@@ -51,8 +65,46 @@ export default {
     align-items: center;
   }
 
-  .eval-cont svg {
-    margin-right: 5px;
+  .bginfo-wrapper {
+    margin-top: 0px;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 448px) {
+    .eval-cont {
+      flex-direction: column;
+    }
+
+    .eval-cont svg {
+      margin-bottom: 5px;
+    }
+
+    .bginfo-wrapper {
+      margin-top: 10px;
+    }
+  }
+
+  @media screen and (min-width: 448px) {
+    .eval-cont {
+      flex-direction: row;
+      justify-content: center;
+    }
+
+    .eval-cont svg {
+      margin-right: 5px;
+    }
+  }
+
+  @media screen and (min-width: 448px) and (max-width: 768px) {
+    .bginfo-wrapper {
+      margin-top: 10px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .bginfo-wrapper {
+      margin-top: 0px;
+    }
   }
 
   a {
